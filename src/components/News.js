f971d053fedfcc179c4fcdac395d47753a64dedd
babaYogii <yogeshkodlinge@gamil.com>
2023-06-18 20:35:37 +0530

    Updated some changes

--- src/components/News.js
+++ src/components/News.js
@@ -43,7 +43,7 @@ const News = (props)=>{
  
         return (
             <>
-                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>RealNews - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
                 <InfiniteScroll
                     dataLength={articles.length}
                     next={fetchMoreData}
